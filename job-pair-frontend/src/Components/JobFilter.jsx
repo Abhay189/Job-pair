5f6514cba1c05bf66b16b582823acdda920d85ea
Abhay Sharma <sabhay2001@gmail.com>
2024-04-03 21:18:39 -0600

    fixing job filters UI

--- job-pair-frontend/src/Components/JobFilter.jsx
+++ job-pair-frontend/src/Components/JobFilter.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import '../Styles/jobfilter.css';
-import { useState } from 'react';
-import { Button } from 'react-bootstrap';
 function Filter({ onFilterChange }) {
-  const [show, setShow] = useState(false);
   return (
-
-    <>
-
-    <Button onClick={()=> {setShow(!show)} } className="filter-button">Show Filter </Button>
     <div className="filter-sidebar desktop">
       <div className="filter-header">
-        Filter
+        Filters
       </div>
       <input
         className="filter-input"
@@ -32,32 +25,6 @@ function Filter({ onFilterChange }) {
         onChange={(e) => onFilterChange('date', e.target.value)}
       />
     </div>
-    {show &&
-    <div className="filter-sidebar">
-      <div className="filter-header">
-        Filter
-      </div>
-      <input
-        className="filter-input"
-        type="text"
-        placeholder="Job Title"
-        onChange={(e) => onFilterChange('title', e.target.value)}
-      />
-      <input
-        className="filter-input"
-        type="text"
-        placeholder="Location"
-        onChange={(e) => onFilterChange('location', e.target.value)}
-      />
-      <input
-        className="filter-input"
-        type="date"
-        placeholder="Posting Date"
-        onChange={(e) => onFilterChange('date', e.target.value)}
-      />
-    </div>}
-    </>
-
   );
 }
 
