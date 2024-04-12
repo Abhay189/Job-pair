e79a99466fb70df18bf52389231739b61b4f6f61
Hassan <hassan.khan1@ucalgary.ca>
2024-04-12 17:30:50 -0600

    feature - mobile application review

--- job-pair-frontend/src/Components/ApplicationReview.jsx
+++ job-pair-frontend/src/Components/ApplicationReview.jsx
@@ -1,34 +1,17 @@
 import { useEffect, useState } from "react";
 import Axios from 'axios';
 import '../Styles/ApplicationReview.css'
-// import { chatgptlogo } from "../Constants";
 import chatgpt from '../Assets/chatgpt.png'
 
 import {
   Container,
-//   DropdownButton,
-//   Dropdown,
-//   Row,
-//   Form,
-//   FormLabel,
-//   Button,
-//   Card,
-//   Modal,
-//   Col,
 } from "react-bootstrap";
-
-// import "../Styles/SalariesPageStyles.css";
-import "../Styles/LearningPageStyles.css";
-// import "../Styles/LearningPageStyles.css"; // Import the new CSS file here
-import { styles } from "../Styles/styles";
 import { useParams } from 'react-router-dom';
 
 export default function ApplicationReview() {
 
-  const { id } = useParams();  // Extracting the id from the route
+  const { id } = useParams();  
 
-  // const param1 = localStorage.getItem("ApplicationReviewTitle");
-  const param1 = "Financial Analyst"
   const [jobName, setJobName] = useState("");
   const [jobDescription, setJobDescription] = useState("");
   const [questions, setQuestions] = useState([]);
@@ -38,29 +21,26 @@ export default function ApplicationReview() {
 
   const redirectToJobs = () => {
     window.location.reload();
-    // Redirects to the main page
     window.location.href = "http://localhost:3000/viewJobs";
-  };
+};
+
 
   useEffect(() => {
-    const jobUrl = `http://127.0.0.1:5002/get_all_jobs?id=${id}&userType=seekers`;  // URL to get job details
+    const jobUrl = `http://127.0.0.1:5002/get_all_jobs?id=${id}&userType=seekers`;
   
     Axios.get(jobUrl, {})
       .then((res) => {
         const jsonData = res.data;
-        console.log("Fetched data:", jsonData);
   
         if (jsonData.length > 0) {
           const jobData = jsonData.find(item => item.id === Number(11));
           if (jobData) {
-            console.log("Job data found:", jobData);
             setJobDescription(jobData.Description);
             setJobName(jobData.Title);
             setQuestions(jobData.Questions);
   
-            // Fetch responses for the job using another endpoint
-            const answersUrl = `http://127.0.0.1:5002/get_job_answer?job_id=11&user_id=1`;  // Include user ID as needed
-            return Axios.get(answersUrl);  // Return the promise for chaining
+            const answersUrl = `http://127.0.0.1:5002/get_job_answer?job_id=11&user_id=1`;
+            return Axios.get(answersUrl);
           } else {
             console.error(`Job with id ${id} not found in jsonData`);
           }
@@ -68,18 +48,15 @@ export default function ApplicationReview() {
       })
       .then((response) => {
         if (response && response.data) {
-          console.log("Responses fetched:", response.data);
-          setResponses(response.data.answers);  // Assuming the endpoint returns an object with an 'answers' key
+          setResponses(response.data.answers);
         }
       })
       .catch((error) => {
         console.error("Error fetching job responses:", error);
       });
-  }, [id]);  // Only id is needed as a dependency now, as we are no longer using jobName
-  
+  }, [id]);  
+
   const handleEditClick = (index) => {
-    console.log('Responses:', responses);
-    console.log('Index:', index);
     if (responses && index < responses.length) {
       setEditingIndex(index);
       setEditedText(responses[index]);
@@ -116,19 +93,16 @@ export default function ApplicationReview() {
 
   const handleEnhanceClick = async (index) => {
     try {
-        // Fetch the enhanced response
         const enhancedResponse = await Axios.post("http://127.0.0.1:5002/get_enhanced_essay", {
             question: questions[index],
             answer: responses[index],
         });
 
-        // Check if we actually received the enhanced response
         if (enhancedResponse && enhancedResponse.data && enhancedResponse.data.response) {
             const updatedResponses = [...responses];
             updatedResponses[index] = enhancedResponse.data.response;
             setResponses(updatedResponses);
 
-            // Save the updated response to the server
             await Axios.post("http://127.0.0.1:5002/update_job_answer", {
                 user_id: 1,  
                 job_id: 11, 
@@ -147,13 +121,11 @@ export default function ApplicationReview() {
 
 
   const handleSubmitClick = async () => {
-    console.log(jobName);
     try {
       const userId = localStorage.getItem('id');
       await Axios.post("http://127.0.0.1:5002/submit_application", {
         id:userId,
         job_id: id,
-        
       });
       
       redirectToJobs()
@@ -167,18 +139,17 @@ export default function ApplicationReview() {
     <div className="container">
       <div style={{ minHeight: `77vh`, textAlign: `left` }}>
         <div className="cardd">
-          <div className={`${styles.heroHeadText}`} style={{ fontWeight: 'bold', fontSize: '50px', textAlign: 'center', marginBottom: '35px', paddingTop: `70px` }}>{jobName}</div>
-          <p className={`${styles.heroHeadText}`} style={{ fontSize: '25px', textAlign: 'center', marginBottom: '35px' }}>{jobDescription}</p>
-          <Container className="outer-container" style={{ minHeight: `100vh`, minWidth:`100vh` }}>
-          <div className="questiondiv"  style={{ minHeight: `100vh`, minWidth:`100vh`, alignContent:`center`, alignItems:`center` }}>
+          <div style={{ fontWeight: 'bold', fontSize: '30px', textAlign: 'center', marginBottom: '20px', paddingTop: `20px` }}>{jobName}</div>
+          <p style={{ fontSize: '20px', textAlign: 'center', marginBottom: '20px' }}>{jobDescription}</p>
+          <Container className="outer-container">
+          <div className="questiondiv">
             {questions.map((questionItem, index) => (
-              <div key={index} className="question-container" style={{ display: 'flex', flexDirection:'column', alignItems: 'center', justifyContent: 'center' }}>
-                <div style={{ width: "50rem", height: "100%", boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', margin: `1% 0`, border:'solid', borderWidth:'4px', borderColor:'#f0f0f0', borderRadius: '15px', padding: '15px', position: 'relative', alignText: 'center', justifyContent: 'space-between'  }}>
-                    <strong className={`${styles.heroHeadText}`} style={{ fontSize: '20px', textAlign: 'center', marginBottom: '35px' }}>Question:</strong> {questions[index]}
-                    <br />
+              <div key={index} className="question-container">
+                <div style={{ width: "100%", boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', margin: `10px 0`, border:'solid', borderWidth:'2px', borderColor:'#f0f0f0', borderRadius: '10px', padding: '10px', position: 'relative' }}>
+                    <strong style={{ fontSize: '20px', textAlign: 'center', marginBottom: '15px' }}>Question:</strong> {questions[index]}
                 </div>
-                <div style={{ width: "50rem", height: "100%", boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', background: '#f0f0f0', margin: `1% 0`, border:'solid', borderWidth:'4px', borderColor:'#f0f0f0', borderRadius: '15px', padding: '15px', position: 'relative', alignText: 'center', justifyContent: 'space-between'  }}>
-                <label htmlFor={`textInput-${index}`} className={`${styles.heroHeadText}`}  style={{ fontSize: '20px', fontWeight:'bold', textAlign: 'center'}} >Answer:</label>
+                <div style={{ width: "100%", boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', background: '#f0f0f0', margin: `10px 0`, border:'solid', borderWidth:'2px', borderColor:'#f0f0f0', borderRadius: '10px', padding: '10px', position: 'relative' }}>
+                <label htmlFor={`textInput-${index}`} style={{ fontSize: '20px', fontWeight:'bold', textAlign: 'center'}} >Answer:</label>
                     {editingIndex === index ? (
                       <div >
                         <textarea 
@@ -189,22 +160,18 @@ export default function ApplicationReview() {
                           rows={4}
                           style={{ width: '100%', background: '#f0f0f0', padding: '10px', boxSizing: 'border-box' }}
                         />
-                        <button className="invisiblePadding saveButton" style={{ marginRight: '10px', fontWeight:'bold', color:'black'  }} onClick={() => handleSaveClick(index)}>Save</button>
+                        <button className="saveButton" style={{ marginRight: '10px', fontWeight:'bold', color:'black'  }} onClick={() => handleSaveClick(index)}>Save</button>
                       </div>
                     ) : (
                       <div>
-                        <div className="answer-container" style={{ overflow: 'hidden' }}>
-                          {/* <p style={{ whiteSpace: 'pre-wrap', overflow: 'hidden' }}>{responses[0]}</p> */}
+                        <div className="answer-container">
                           <p style={{ whiteSpace: 'pre-wrap', overflow: 'hidden' }}>{responses[index]}</p>
                         </div>
-                        <div className="buttonContainer" style={{ display: 'flex', flexDirection: 'row', alignItems: 'left', justifyContent: 'left' }}>
-                          <span style={{ padding: '0 8px' }}></span>
-                          <button className="invisiblePadding editButton" style={{ height: '35px', marginRight: '10px', fontWeight: 'bold', color: 'white', backgroundColor: '#575DFB', borderRadius: '15px', border: 'none', }} onClick={() => handleEditClick(index)}>Edit</button>
-                          <span style={{ padding: '0 8px' }}></span>
-                          <button className="invisiblePadding enhanceButton" style={{ width: '140px', marginRight: '10px', fontWeight: 'bold', color: 'white', backgroundColor: '#575DFB', borderRadius: '15px', border: 'none', display: 'flex', flex: 'wrap' }} onClick={() => handleEnhanceClick(index)}>
-                            <img src={chatgpt} alt="ChatGPT Icon" style={{ marginRight: '5px', height: '30px', width: '30px' }} /> Enhance
+                        <div className="buttonContainer">
+                          <button className="editButton" style={{ marginRight: '10px', fontWeight: 'bold', color: 'white', backgroundColor: '#575DFB', borderRadius: '10px', border: 'none' }} onClick={() => handleEditClick(index)}>Edit</button>
+                          <button className="enhanceButton" style={{ fontWeight: 'bold', color: 'white', backgroundColor: '#575DFB', borderRadius: '10px', border: 'none' }} onClick={() => handleEnhanceClick(index)}>
+                            <img src={chatgpt} alt="ChatGPT Icon" style={{ marginRight: '5px', height: '20px', width: '20px' }} /> Enhance
                           </button>
-                          <span style={{ padding: '0 8px' }}></span>
                         </div>
                       </div>
                     )}
@@ -215,14 +182,14 @@ export default function ApplicationReview() {
           </div>
           </Container>
           <div style={{ textAlign: "center" }}>
-            <button className="SignupButton"   style={{
+            <button className="SignupButton" style={{
                     backgroundColor: `#575DFB`,
                     color: `black`,
                     border: `none`,
-                    width:'400px',
-                    borderRadius: '15px',
+                    width:'200px',
+                    borderRadius: '10px',
                     fontWeight:'bold',
-                    marginBottom:'40px',
+                    marginBottom:'20px',
                   }} onClick={handleSubmitClick}>
               Submit
             </button>
